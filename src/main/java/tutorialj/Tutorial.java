686600bb065887f776b521186953b7ba4bf9b821
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2013-12-23 19:40:37 -0800

    Documenting link feature

--- src/main/java/tutorialj/Tutorial.java
+++ src/main/java/tutorialj/Tutorial.java
@@ -15,8 +15,24 @@ import java.lang.annotation.Target;
 @Tutorial(order=3,showSource=true)
 public @interface Tutorial
 {
+  /**
+   * @return Whether the source code should be shown in this tutorial step
+   */
   boolean showSource() default false;
+  
+  /**
+   * @return The index in the order of presentation (typically an integer,
+   * but double can be useful to insert intermediate steps)
+   */
   double order() default 0.0; // TODO: for methods/fields/constructors, make it inherit order in file by default
+  
+  /**
+   * @return A path prefix to be used if a link to the source code is generated
+   */
   String linkPrefix() default "src/main/java/";
+  
+  /**
+   * @return Whether a link to the source code should be shown in this tutorial step
+   */
   boolean showLink() default false;
 }
